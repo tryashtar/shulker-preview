469091cd864ce908fae60e513df3064fbb0cd64a
tryashtar <tryashtar@gmail.com>
2018-12-29 23:38:12 -0700

    Block icons too

--- script.py
+++ script.py
@@ -52,15 +52,23 @@ def get_digit_char(digit, row):
 import os
 import math
 from PIL import Image
-path = "D:\\Minecraft\\Java Storage\\Game History\\jar\\assets\\minecraft\\textures\\item"
+items_path = "D:\\Minecraft\\Java Storage\\Game History\\jar\\assets\\minecraft\\textures\\item"
+blocks_path = "..\\block images"
 
 # get all item PNGs from minecraft files
 items = []
-for file in os.listdir(path):
+blocks = []
+all_items = []
+for file in os.listdir(items_path):
     if include(file):
       items.append(file)
+      all_items.append(file)
+for file in os.listdir(blocks_path):
+    if include(file):
+      blocks.append(file)
+      all_items.append(file)
 
-# create spritesheet
+# create spritesheets
 size = 16*math.ceil(math.sqrt(len(items)))
 itemgrid = []
 itemrow = []
@@ -68,7 +76,8 @@ x = 0
 y = 0
 with Image.new("RGBA", (size, size-16)) as sheet:
    for item in items:
-      with Image.open(os.path.join(path, item)).convert("RGBA") as sprite:
+      image_path = os.path.join(items_path, item)
+      with Image.open(image_path).convert("RGBA") as sprite:
          pixels = sprite.load()
          r,g,b,a = pixels[0,0]
          a = max(a,1)
@@ -89,30 +98,63 @@ with Image.new("RGBA", (size, size-16)) as sheet:
    itemgrid.append(itemrow.copy())
    sheet.save("resourcepack\\assets\\shulker_item\\textures\\item_sheet.png", "PNG")
 
+size = 64*math.ceil(math.sqrt(len(blocks)))
+blockgrid = []
+blockrow = []
+x = 0
+y = 0
+with Image.new("RGBA", (size, size-64)) as sheet:
+   for block in blocks:
+      image_path = os.path.join(blocks_path, block)
+      with Image.open(image_path).convert("RGBA") as sprite:
+         pixels = sprite.load()
+         r,g,b,a = pixels[0,0]
+         a = max(a,1)
+         pixels[0,0] = (r,g,b,a)
+         r,g,b,a = pixels[63,63]
+         a = max(a,1)
+         pixels[63,63] = (r,g,b,a)
+         sheet.paste(sprite, (x, y, x+64, y+64))
+      x += 64
+      blockrow.append(block)
+      if x >= size:
+         x = 0
+         y += 64
+         blockgrid.append(blockrow.copy())
+         blockrow = []
+   while len(blockrow) < len(blockgrid[0]):
+      blockrow.append("")
+   blockgrid.append(blockrow.copy())
+   sheet.save("resourcepack\\assets\\shulker_item\\textures\\block_sheet.png", "PNG")
+
 # create font characters for each
 with open("resourcepack\\assets\\minecraft\\font\\default.json", "w") as file:
    num = 31
    file.write("{\"providers\":[{\"type\":\"ttf\",\"file\":\"shulker_item:negative_spaces.ttf\",\"shift\":[0.0,0.0],\"size\":10.0,\"oversample\":1.0},{\"type\":\"bitmap\",\"file\":\"shulker_item:tooltip.png\",\"ascent\":23,\"height\":78,\"chars\":[\"\\uE000\"]},{\"type\":\"bitmap\",\"file\":\"shulker_item:numbers.png\",\"ascent\":-4,\"height\":8,\"chars\":[\"\\uE001\\uE002\\uE003\\uE004\\uE005\\uE006\\uE007\\uE008\\uE009\\uE00A\"]},{\"type\":\"bitmap\",\"file\":\"shulker_item:numbers.png\",\"ascent\":-22,\"height\":8,\"chars\":[\"\\uE00B\\uE00C\\uE00D\\uE00E\\uE00F\\uE010\\uE011\\uE012\\uE013\\uE014\"]},{\"type\":\"bitmap\",\"file\":\"shulker_item:numbers.png\",\"ascent\":-40,\"height\":8,\"chars\":[\"\\uE015\\uE016\\uE017\\uE018\\uE019\\uE01A\\uE01B\\uE01C\\uE01D\\uE01E\"]},")
    for i in range(0, 3):
-      ascent = 5-(18*i)
-      file.write("{\"type\":\"bitmap\",\"file\":\"shulker_item:item_sheet.png\",\"ascent\":" +str(ascent)+ ",\"height\":16,\"chars\":[")
-      last = len(itemgrid)
-      current = 0
-      for itemrow in itemgrid:
-         current += 1
-         file.write("\"")
-         for item in itemrow:
-            if item == "":
-               file.write("\\u0000")
-            else:
-               file.write("\\uE" +format(num,"03x").upper())
-               num += 1
-         file.write("\"")
-         if current < last:
+      for thing in ["item", "block"]:
+         grid = itemgrid
+         if thing == "block":
+            grid = blockgrid
+         ascent = 5-(18*i)
+         file.write("{\"type\":\"bitmap\",\"file\":\"shulker_item:" +thing+ "_sheet.png\",   \"ascent\":" +str(ascent)+ ",\"height\":16,\"chars\":[")
+         last = len(grid)
+         current = 0
+         for row in grid:
+            current += 1
+            file.write("\"")
+            for item in row:
+               if item == "":
+                  file.write("\\u0000")
+               else:
+                  file.write("\\uE" +format(num,"03x").upper())
+                  num += 1
+            file.write("\"")
+            if current < last:
+               file.write(",")
+         file.write("]}")
+         if i<2 or thing == "item":
             file.write(",")
-      file.write("]}")
-      if i<2:
-         file.write(",")
    file.write("]}")
 
 # create translations for each
@@ -123,8 +165,11 @@ with open("resourcepack\\assets\\minecraft\\lang\\en_us.json", "w") as file:
       for number in range(2, 65):
          file.write("\"shulker_item.number." +str(number)+ "." + str(i) + "\":\"\\uF820" +get_number_translation(number, i)+ "\\uF800\",")
       for item in items:
-         file.write("\"shulker_item.item." +item_name(item)+ "." +str(i)+ "\":\"\\uF820\\uE" +format(num,"03x").upper()+ "\\uF805\\uF800\"")
-         if i<2 or item != items[-1]:
+         file.write("\"shulker_item.item." +item_name(item)+ "." +str(i)+ "\":\"\\uF820\\uE" +format(num,"03x").upper()+ "\\uF805\\uF800\",")
+         num += 1
+      for block in blocks:
+         file.write("\"shulker_item.block." +item_name(block)+ "." +str(i)+ "\":\"\\uF820\\uE" +format(num,"03x").upper()+ "\\uF805\\uF800\"")
+         if i<2 or block != blocks[-1]:
             file.write(",")
          num += 1
    file.write("}")
@@ -136,6 +181,8 @@ with open("datapack\\data\\shulker_item\\functions\\process_box.mcfunction", "w"
       row = i//9
       for item in items:
          file.write("execute if data block ~ ~1 ~ Items[{Slot:" +str(i)+ "b,id:\"minecraft:" +item_name(item)+ "\"}] run summon area_effect_cloud ~ ~" +str(i+4)+ " ~ {Tags:[\"shulker_item\"],CustomName:\"{\\\"translate\\\":\\\"shulker_item.item." +item_name(item)+ "." +str(row)+ "\\\"}\"}\n")
+      for block in blocks:
+         file.write("execute if data block ~ ~1 ~ Items[{Slot:" +str(i)+ "b,id:\"minecraft:" +item_name(block)+ "\"}] run summon area_effect_cloud ~ ~" +str(i+4)+ " ~ {Tags:[\"shulker_item\"],CustomName:\"{\\\"translate\\\":\\\"shulker_item.block." +item_name(block)+ "." +str(row)+ "\\\"}\"}\n")
       file.write("execute unless data block ~ ~1 ~ Items[{Slot:" +str(i)+ "b}] run summon area_effect_cloud ~ ~" +str(i+4)+ " ~ {Tags:[\"shulker_item\"],CustomName:\"{\\\"translate\\\":\\\"shulker_item.empty_slot\\\"}\"}\nexecute store result score #count shulker_item run data get block ~ ~1 ~ Items[{Slot:" +str(i)+ "b}].Count\nexecute positioned ~ ~" +str(i+4.1)+ " ~ run function shulker_item:process_count_" +str(row)+ "\n")
       if (i+1)%9 == 0:
          file.write("summon area_effect_cloud ~ ~" +str(i+4.2)+ " ~ {Tags:[\"shulker_item\"],CustomName:\"{\\\"translate\\\":\\\"shulker_item.row_end\\\"}\"}\n")
@@ -147,3 +194,28 @@ with open("datapack\\data\\shulker_item\\functions\\process_box\\0.mcfunction", 
    for i in range(1, 27):
       with open("datapack\\data\\shulker_item\\functions\\process_box\\" +str(i)+ ".mcfunction", "w") as file2:
          file2.write(text.replace("0", str(i)))
+
+# test all items!
+import json
+with open("D:\\Minecraft\\Java Storage\\Game History\\reports\\registries.json") as file:
+   register = json.loads(file.read())
+   mc_items = list(register["minecraft:item"]["entries"].keys())
+   index = 0
+   while index < len(mc_items):
+      boxes = 0
+      command = "setblock ~ ~1 ~ chest{Items:["
+      while index < len(mc_items):
+         item = mc_items[index]
+         if index%27 == 0:
+            command += "{id:shulker_box,Count:1b,Slot:" +str(boxes)+ "b,tag:{BlockEntityTag:{Items:["
+            boxes += 1
+         command += "{id:\"" +item[len("minecraft:"):]+ "\",Count:1b,Slot:" +str(index%27)+ "b},"
+         index += 1
+         if index%27 == 0:
+            command += "]}}},"
+            if boxes >= 27:
+               break
+      if index%27 != 0:
+         command += "]}}},"
+      command += "]}"
+      print(command)
