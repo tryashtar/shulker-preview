28931632a32b96b836686c914170c4432c6595e1
tryashtar <tryashtar@gmail.com>
2022-05-28 00:36:50 -0600

    Bugfixes and restore necessary translations

--- 1.19/script.py
+++ 1.19/script.py
@@ -21,6 +21,7 @@ def main():
    # it would be a waste of time to find the server jar and generate them in this script
    data = urlopen(f'https://raw.githubusercontent.com/misode/mcmeta/{version}-summary/registries/data.json').read()
    item_registry = json.loads(data)["item"]
+   item_registry.remove('air')
    jar_path = find_version_jar()
    mc = Minecraft(jar_path, item_registry, read_json('data.json'))
    font = FontAbstraction(3)
@@ -36,14 +37,23 @@ def main():
    #   - create from actual textures and draw including shadows
    # - durability
    #   - still using texture grid for this, need a way to create chars/translations
-   #   - how to reference function if it's created in generate_functions but used in generate_item_lines?
    # - potion/arrow/map overlays
+   #   - first generate the "normal" command if this item was not special at all
+   #   - but instead of writing it, write a function that checks for the colors and replaces color in normal command with appropriate color
    # - banner/shield overlays
    #   - need to use old method of generating grid from screenshots, doubt shaders can do this one
    # - dyed armor color
-   #   - still needs overlays
+   #   - just like the other one, generate "normal" command but insert a place to colorize layer0
    # - all the overrides like light blocks
    # - generate test command
+   # - slice ender chest tooltip
+   # - sort output files again
+   # - shader version of resource pack:
+   #   - copy item name to one of the empty lore rows, with color that shader uses to push it upward
+   #   - second %s makes it visible only in this pack
+   # - non-shader version of resource pack:
+   #   - still use custom textures for tooltips
+   #   - old grid method for blocks
 
 # drawing an image as text with zero width requires multiple characters in the font
 class FontAbstraction:
@@ -91,36 +101,49 @@ class FontAbstraction:
       lang = {"%1$s%418634357$s":"%2$s"}
       # create the tooltip characters
       # the tooltip is made by slicing the vanilla texture into rows and assembling them
-      tooltip_neg = self.next_char()
       tooltip_1 = self.next_char()
+      tooltip_neg1 = self.next_char()
       tooltip_2 = self.next_char()
+      tooltip_neg2 = self.next_char()
       tooltip_3 = self.next_char()
+      tooltip_neg3 = self.next_char()
       split_2 = ['\u0000'] * 32
+      split_neg2 = ['\u0000'] * 32
       split_3 = ['\u0000'] * 32
+      split_neg3 = ['\u0000'] * 32
       split_2[8] = tooltip_2
+      split_neg2[8] = tooltip_neg2
       split_3[20] = tooltip_3
+      split_neg3[20] = tooltip_neg3
       font = [
-         # negative version, used to bring the cursor back to draw the next row
-         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": -32768, "height": -260, "chars": [tooltip_neg]},
          # top: the first two rows of slots and most of the third
          # we can only slice by whole numbers, and a slice any larger would be too big
          {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": 23, "height": 64, "chars": [tooltip_1, "\u0000", "\u0000", "\u0000"]},
+         # negative version, used to bring the cursor back to draw the next row
+         # in theory we could reuse this for each row, but it breaks if the slices are different widths
+         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": -32768, "height": -64, "chars": [tooltip_neg1, "\u0000", "\u0000", "\u0000"]},
          # middle: the rest of the third row
          {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": 23-64, "height": 8, "chars": split_2},
+         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": -32768, "height": -8, "chars": split_neg2},
          # bottom: the very bottom of the tooltip texture
-         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": 23-64-8, "height": 8, "chars": split_3}
+         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": 23-64-8, "height": 8, "chars": split_3},
+         {"type": "bitmap", "file": "minecraft:gui/container/shulker_box.png", "ascent": -32768, "height": -8, "chars": split_neg3}
       ]
       # start by moving back 6 pixels to fully cover the vanilla tooltip
       # then each row and negative in turn
-      # then forward 7 pixels, aligning the first item to draw with the first slot
-      lang["tryashtar.shulker_preview.shulker_tooltip"] = self.get_space(-6) + tooltip_1 + tooltip_neg + tooltip_2 + tooltip_neg + tooltip_3 + tooltip_neg + self.get_space(7)
-      for s, ch in self.spaces.items():
-         font.append({"type": "bitmap", "file": "tryashtar.shulker_preview:pixel.png", "ascent": -32768, "height": s, "chars": [ch]})
+      # then forward a few pixels, aligning the first item to draw with the first slot
+      lang["tryashtar.shulker_preview.shulker_tooltip"] = self.get_space(-6) + tooltip_1 + tooltip_neg1 + self.get_space(-5) + tooltip_2 + tooltip_neg2 + self.get_space(-5) + tooltip_3 + tooltip_neg3 + self.get_space(4)
+      lang["tryashtar.shulker_preview.shulker_tooltip_header"] = lang["tryashtar.shulker_preview.shulker_tooltip"]
+      lang["tryashtar.shulker_preview.empty_slot"] = self.get_space(17)
+      lang["tryashtar.shulker_preview.overlay"] = self.get_space(-20)
+      lang["tryashtar.shulker_preview.row_end"] = self.get_space(-164)
       for t, data in self.textures.items():
          for i in range(0, self.rows):
-            lang[self.translations[i][t]] = self.characters[i][t] + self.negatives[t]
+            lang[self.translations[i][t]] = self.characters[i][t] + self.negatives[t] + self.get_space(14)
             font.append({"type": "bitmap", "file": t + '.png', "ascent": data['ascent'] - (18 * i), "height": data['height'], "chars": [self.characters[i][t]]})
          font.append({"type": "bitmap", "file": t + '.png', "ascent": -32768, "height": -data['height'], "chars": [self.negatives[t]]})
+      for s, ch in self.spaces.items():
+         font.append({"type": "bitmap", "file": "tryashtar.shulker_preview:space.png", "ascent": -32768, "height": s, "chars": [ch]})
       write_json(lang, os.path.join(path, 'assets/tryashtar.shulker_preview/lang/en_us.json'))
       write_json({"providers":font}, os.path.join(path, 'assets/tryashtar.shulker_preview/font/preview.json'))
 
@@ -143,10 +166,10 @@ class Minecraft:
       self.textures = []
       self.items = item_registry
       self.length_dict = {}
-      self.hardcoded_tints = data['hardcoded_tints']
-      for k,v in data['spawn_eggs'].items():
-         self.hardcoded_tints['layer0'][k] = color_hex(v[0])
-         self.hardcoded_tints['layer1'][k] = color_hex(v[1])
+      self.data = data
+      for k,v in self.data['spawn_eggs'].items():
+         self.data['hardcoded_tints']['layer0'][k] = color_hex(v[0])
+         self.data['hardcoded_tints']['layer1'][k] = color_hex(v[1])
       for item in self.items:
          namespaced = add_namespace(item)
          length = len(namespaced)
@@ -274,14 +297,15 @@ def color_hex(int_color):
 
 def generate_functions(mc, font, path):
    for row in range(0, font.rows):
-      process_item = Function(f'tryashtar.shulker_preview:row_{row}/process_item', [
+      fn_start = f'tryashtar.shulker_preview:row_{row}'
+      process_item = Function(f'{fn_start}/process_item', [
          '# get the length of this item and call the appropriate function',
          'execute store result score #length shulker_preview run data get storage tryashtar.shulker_preview:data item.id'
       ])
       lengths = list(mc.length_dict.keys())
       lengths.sort()
       for length in range(0,100):
-         subfunction = Function(f'tryashtar.shulker_preview:row_{row}/process_item/length_{length}', [])
+         subfunction = Function(f'{fn_start}/process_item/length_{length}', [])
          if length in lengths:
             process_item.lines.append(f'execute if score #length shulker_preview matches {length} run function {subfunction.resource}')
             subfunction.lines.extend(generate_item_lines(mc, mc.length_dict[length], row, font))
@@ -290,7 +314,7 @@ def generate_functions(mc, font, path):
             fpath = os.path.join(path, subfunction.file_path)
             if os.path.exists(fpath):
                os.remove(fpath)
-      count = Function(f'tryashtar.shulker_preview:row_{row}/overlay/count', ['# create an entity that draws item counts'])
+      count = Function(f'{fn_start}/overlay/count', ['# create an entity that draws item counts'])
       for i in range(2, 65):
          n = str(i) if i < 64 else f"{i}.."
          count.lines.append(f'execute if score #count shulker_preview matches {n} run summon marker ~ ~0.9 ~ {{Tags:["tryashtar.shulker_preview"],CustomName:\'{{"translate":"tryashtar.shulker_preview.number.{i}.{row}"}}\'}}')
@@ -305,6 +329,26 @@ def generate_functions(mc, font, path):
          f'execute if score #count shulker_preview matches 2.. run function {count.resource}'
       ])
       process_item.save(path)
+
+      # durability overlay
+      durability = Function(f'{fn_start}/overlay/durability', [
+         '# create an entity that draws a durability bar',
+         'scoreboard players operation #durability shulker_preview *= #13000 shulker_preview',
+         'scoreboard players operation #durability shulker_preview /= #max shulker_preview'
+      ])
+      for i in range(1, 15):
+         text = f'execute if score #durability shulker_preview matches '
+         lower = (1000 * i) - 1500
+         upper = (1000 * i) - 501
+         if i == 14:
+            text += f'{lower}..'
+         elif i == 1:
+            text += f'1..{upper}'
+         else:
+            text += f'{lower}..{upper}'
+         text += f' run summon marker ~ ~0.8 ~ {{Tags:["tryashtar.shulker_preview"],CustomName:\'{{"translate":"tryashtar.shulker_preview.durability.{i}.{row}"}}\'}}'
+         durability.lines.append(text)
+      durability.save(path)
 
 def generate_item_lines(mc, items, row, font):
    lines = []
@@ -327,12 +371,14 @@ def generate_item_lines(mc, items, row, font):
                break
             font.add_texture(texture, 5, 16)
             component = {"translate":font.get_translation(texture, row)}
-            hardcoded = mc.hardcoded_tints.get(f'layer{i}', {}).get(item)
+            hardcoded = mc.data['hardcoded_tints'].get(f'layer{i}', {}).get(item)
             if hardcoded is not None:
                component["color"] = hardcoded
             # prevent color from bleeding across components
             elif i > 0 and name[0] != "" and name[0].get("color") is not None:
                name.insert(0, "")
+            if i > 0:
+               name.append({"translate":"tryashtar.shulker_preview.overlay"})
             name.append(component)
          if len(name) == 0:
             print(f'{item} has no layers?')
