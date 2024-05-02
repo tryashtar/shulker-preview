7569347b48e51120eb962991328e4d2db57f12a1
tryashtar <tryashtar@gmail.com>
2024-05-02 02:00:56 -0600

    Work on colorable items

--- 1.20/script.py
+++ 1.20/script.py
@@ -21,6 +21,8 @@ def main():
    negative_tooltip = get_space(space_provider, char_cache, -169)
    empty_slot = get_space(space_provider, char_cache, 18)
    next_slot = get_space(space_provider, char_cache, 15)
+   overlay_offset = get_space(space_provider, char_cache, -3)
+   one_space = get_space(space_provider, char_cache, 3)
    row_end = get_space(space_provider, char_cache, -162)
    lang = {"tryashtar.shulker_preview.translate":"%2$s"}
    tooltip1 = next_char(char_cache)
@@ -35,15 +37,31 @@ def main():
    special_render_tag = []
    hardcoded_items = {
       'grass_colored': {"vine":0x48b518,"lily_pad":0x71c35c,"short_grass":0x7bbd6b,"fern":0x7bbd6b,"tall_grass":0x7bbd6b,"large_fern":0x7bbd6b},
-      'spawn_eggs': {f'{k}_spawn_egg':v for k,v in {"bogged":(9084018,3231003),"armadillo":(11366765,0x824848),"wither":(0x141414,5075616),"snow_golem":(14283506,8496292),"sniffer":(8855049,2468720),"iron_golem":(14405058,7643954),"ender_dragon":(0x1C1C1C,14711290),"camel":(16565097,13341495),"allay":(56063,44543),"axolotl":(16499171,10890612),"bat":(4996656,986895),"bee":(15582019,4400155),"blaze":(16167425,16775294),"breeze":(11506911,9529055),"cat":(15714446,9794134),"cave_spider":(803406,11013646),"chicken":(10592673,16711680),"cod":(12691306,15058059),"cow":(4470310,10592673),"creeper":(894731,0),"dolphin":(2243405,16382457),"donkey":(5457209,8811878),"drowned":(9433559,7969893),"elder_guardian":(13552826,7632531),"enderman":(1447446,0),"endermite":(1447446,7237230),"evoker":(9804699,1973274),"frog":(13661252,0xFFC77C),"fox":(14005919,13396256),"ghast":(16382457,12369084),"glow_squid":(611926,8778172),"goat":(10851452,5589310),"guardian":(5931634,15826224),"hoglin":(13004373,6251620),"horse":(12623485,15656192),"husk":(7958625,15125652),"llama":(12623485,10051392),"magma_cube":(3407872,16579584),"mooshroom":(10489616,12040119),"mule":(1769984,5321501),"ocelot":(15720061,5653556),"panda":(15198183,1776418),"parrot":(894731,16711680),"phantom":(4411786,8978176),"pig":(15771042,14377823),"piglin":(10051392,16380836),"piglin_brute":(5843472,16380836),"pillager":(5451574,9804699),"polar_bear":(0xEEEEDE,14014157),"pufferfish":(16167425,3654642),"rabbit":(10051392,7555121),"ravager":(7697520,5984329),"salmon":(10489616,951412),"sheep":(15198183,16758197),"shulker":(9725844,5060690),"silverfish":(7237230,3158064),"skeleton":(12698049,4802889),"skeleton_horse":(6842447,15066584),"slime":(5349438,8306542),"spider":(3419431,11013646),"squid":(2243405,7375001),"stray":(6387319,14543594),"strider":(10236982,5065037),"tadpole":(7164733,1444352),"trader_llama":(15377456,4547222),"tropical_fish":(15690005,16775663),"turtle":(15198183,44975),"vex":(8032420,15265265),"villager":(5651507,12422002),"vindicator":(9804699,2580065),"wandering_trader":(4547222,15377456),"warden":(1001033,3790560),"witch":(3407872,5349438),"wither_skeleton":(1315860,4672845),"wolf":(14144467,13545366),"zoglin":(13004373,15132390),"zombie":(44975,7969893),"zombie_horse":(3232308,9945732),"zombified_piglin":(15373203,5009705),"zombie_villager":(5651507,7969893)}.items()},
-      'colored': ["leather_helmet","leather_chestplate","leather_leggings","leather_boots","leather_horse_armor","potion","splash_potion","lingering_potion","firework_star","filled_map","tipped_arrow"]
+      'spawn_eggs': {f'{k}_spawn_egg':v for k,v in {"bogged":(9084018,3231003),"armadillo":(11366765,0x824848),"wither":(0x141414,5075616),"snow_golem":(14283506,8496292),"sniffer":(8855049,2468720),"iron_golem":(14405058,7643954),"ender_dragon":(0x1C1C1C,14711290),"camel":(16565097,13341495),"allay":(56063,44543),"axolotl":(16499171,10890612),"bat":(4996656,986895),"bee":(15582019,4400155),"blaze":(16167425,16775294),"breeze":(11506911,9529055),"cat":(15714446,9794134),"cave_spider":(803406,11013646),"chicken":(10592673,16711680),"cod":(12691306,15058059),"cow":(4470310,10592673),"creeper":(894731,0),"dolphin":(2243405,16382457),"donkey":(5457209,8811878),"drowned":(9433559,7969893),"elder_guardian":(13552826,7632531),"enderman":(1447446,0),"endermite":(1447446,7237230),"evoker":(9804699,1973274),"frog":(13661252,0xFFC77C),"fox":(14005919,13396256),"ghast":(16382457,12369084),"glow_squid":(611926,8778172),"goat":(10851452,5589310),"guardian":(5931634,15826224),"hoglin":(13004373,6251620),"horse":(12623485,15656192),"husk":(7958625,15125652),"llama":(12623485,10051392),"magma_cube":(3407872,16579584),"mooshroom":(10489616,12040119),"mule":(1769984,5321501),"ocelot":(15720061,5653556),"panda":(15198183,1776418),"parrot":(894731,16711680),"phantom":(4411786,8978176),"pig":(15771042,14377823),"piglin":(10051392,16380836),"piglin_brute":(5843472,16380836),"pillager":(5451574,9804699),"polar_bear":(0xEEEEDE,14014157),"pufferfish":(16167425,3654642),"rabbit":(10051392,7555121),"ravager":(7697520,5984329),"salmon":(10489616,951412),"sheep":(15198183,16758197),"shulker":(9725844,5060690),"silverfish":(7237230,3158064),"skeleton":(12698049,4802889),"skeleton_horse":(6842447,15066584),"slime":(5349438,8306542),"spider":(3419431,11013646),"squid":(2243405,7375001),"stray":(6387319,14543594),"strider":(10236982,5065037),"tadpole":(7164733,1444352),"trader_llama":(15377456,4547222),"tropical_fish":(15690005,16775663),"turtle":(15198183,44975),"vex":(8032420,15265265),"villager":(5651507,12422002),"vindicator":(9804699,2580065),"wandering_trader":(4547222,15377456),"warden":(1001033,3790560),"witch":(3407872,5349438),"wither_skeleton":(1315860,4672845),"wolf":(14144467,13545366),"zoglin":(13004373,15132390),"zombie":(44975,7969893),"zombie_horse":(3232308,9945732),"zombified_piglin":(15373203,5009705),"zombie_villager":(5651507,7969893)}.items()}
    }
-   special_render_functions = {k:[[],[],[]] for k in hardcoded_items.keys()}
+   colorable_items = {"potion":["potion","splash_potion","lingering_potion","tipped_arrow"],"star":["firework_star"],"map":["filled_map"]}
+   init_data = []
    for name,values in hardcoded_items.items():
-      values = values.keys() if isinstance(values, dict) else values
-      write_json({"values":list(sorted(values))}, f'datapack/data/tryashtar.shulker_preview/tags/items/special_render/{name}.json')
+      write_json({"values":list(sorted(values.keys()))}, f'datapack/data/tryashtar.shulker_preview/tags/items/special_render/{name}.json')
       special_render_tag.append(f'#tryashtar.shulker_preview:special_render/{name}')
+      for item,color in values.items():
+         if isinstance(color, tuple):
+            init_data.append(f'"minecraft:{item}":{{base:"{color_hex(color[0])}",overlay:"{color_hex(color[1])}"}}')
+         else:
+            init_data.append(f'"minecraft:{item}":{{color:"{color_hex(color)}"}}')
+   special_render_tag.append("#dyeable")
+   for kind,items in colorable_items.items():
+      if len(items) > 1:
+         write_json({"values":list(sorted(items))}, f'datapack/data/tryashtar.shulker_preview/tags/items/special_render/{kind}.json')
+         special_render_tag.append(f'#tryashtar.shulker_preview:special_render/{kind}')
+      else:
+         special_render_tag.extend(items)
+
+   init_data = ','.join(init_data)
+   write_lines([f'data modify storage tryashtar.shulker_preview:data colors set value {{{init_data}}}'], 'datapack/data/tryashtar.shulker_preview/functions/meta/initialize_data.mcfunction')
    with zipfile.ZipFile(jar_path, 'r') as jar:
+      with io.TextIOWrapper(jar.open('data/minecraft/tags/items/dyeable.json'), encoding='utf-8') as model_file:
+         vanilla_dyeables = json.load(model_file)['values']
       def load_textures(item, model):
          result = {'base':[],'overrides':[]}
          if model.get('generated', False):
@@ -70,18 +88,16 @@ def main():
          model = get_model(jar, model_cache, f'item/{item}')
          textures = load_textures(item, model)
          if len(textures['base']) > 0:
-            if item in hardcoded_items['grass_colored']:
-               functions = special_render_functions['grass_colored']
-               translations = add_normal_translations(item, textures['base'], lang, next_slot)
-               color = hardcoded_items['grass_colored'][item]
-               for row,lines in enumerate(functions):
-                  lines.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents {item} run data modify storage tryashtar.shulker_preview:data tooltip append value \'{{"translate":"{translations[row]}","color":"{color_hex(color)}"}}\'')
-            elif item.endswith('_spawn_egg'):
-               functions = special_render_functions['spawn_eggs']
-               translations = add_layered_translations('spawn_egg', textures['base'], lang, next_slot)
-               color = hardcoded_items['spawn_eggs'][item]
-               for row,lines in enumerate(functions):
-                  lines.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents {item} run data modify storage tryashtar.shulker_preview:data tooltip append value \'[{{"translate":"{translations[row][0]}","color":"{color_hex(color[0])}"}},{{"translate":"{translations[row][1]}","color":"{color_hex(color[1])}"}},{{"translate":"tryashtar"}}]\'')
+            if item.endswith('_spawn_egg'):
+               if item not in hardcoded_items['spawn_eggs']:
+                  print(f'WARNING: {item} missing colors')
+               add_layered_translations('spawn_egg', textures['base'], lang, next_slot, overlay_offset)
+            elif with_namespace(item) in vanilla_dyeables:
+               if item in ('leather_chestplate', 'leather_horse_armor'):
+                  if len(textures['base']) < 2:
+                     textures['base'].append(None)
+                  textures['base'][1] = {'rows':[one_space, one_space, one_space], 'negative': ''}
+               add_layered_translations(with_namespace(item), textures['base'], lang, next_slot, overlay_offset)
             else:
                add_normal_translations(item, textures['base'], lang, next_slot)
          else:
@@ -94,7 +110,7 @@ def main():
    for row in range(0, 3):
       process_item = [
          'data modify entity 7368756c-6b65-7220-7072-657669657721 Item set from storage tryashtar.shulker_preview:data item',
-         f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents #tryashtar.shulker_preview:special_render run function tryashtar.shulker_preview:row_{row}/special_render',
+         f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents #tryashtar.shulker_preview:special_render run function tryashtar.shulker_preview:row_{row}/special_render with storage tryashtar.shulker_preview:data item',
          f'execute unless items entity 7368756c-6b65-7220-7072-657669657721 contents #tryashtar.shulker_preview:special_render run function tryashtar.shulker_preview:row_{row}/simple_render with storage tryashtar.shulker_preview:data item',
          f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents *[damage~{{damage:{{min:1}}}},max_damage] run function tryashtar.shulker_preview:row_{row}/overlay/durability',
          f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents *[count~{{min:2}}] run function tryashtar.shulker_preview:row_{row}/overlay/count'
@@ -102,12 +118,31 @@ def main():
       simple_render = [
          f'$data modify storage tryashtar.shulker_preview:data tooltip append value \'{{"translate":"tryashtar.shulker_preview.item.$(id).{row}","fallback":"%s","with":[{{"translate":"tryashtar.shulker_preview.missingno.{row}"}}]}}\''
       ]
-      special_render = []
+      special_render = [
+         '$data modify storage tryashtar.shulker_preview:data item merge from storage tryashtar.shulker_preview:data colors."$(id)"'
+      ]
       for name in hardcoded_items:
-         special_render.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 #tryashtar.shulker_preview:special_render/{name} run function tryashtar.shulker_preview:row_{row}/special_render/{name}')
+         special_render.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents #tryashtar.shulker_preview:special_render/{name} run function tryashtar.shulker_preview:row_{row}/special_render/{name} with storage tryashtar.shulker_preview:data item')
+      special_render.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents #dyeable run function tryashtar.shulker_preview:row_{row}/special_render/dyeable with storage tryashtar.shulker_preview:data item')
+      for kind,items in colorable_items.items():
+         if len(items) == 1:
+            special_render.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents {items[0]} run function tryashtar.shulker_preview:row_{row}/special_render/{kind}')
+         else:
+            special_render.append(f'execute if items entity 7368756c-6b65-7220-7072-657669657721 contents #tryashtar.shulker_preview:special_render/{kind} run function tryashtar.shulker_preview:row_{row}/special_render/{kind}')
       write_lines(process_item, f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/process_item.mcfunction')
       write_lines(simple_render, f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/simple_render.mcfunction')
       write_lines(special_render, f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/special_render.mcfunction')
+      write_lines([
+         f'$data modify storage tryashtar.shulker_preview:data tooltip append value \'{{"translate":"tryashtar.shulker_preview.item.$(id).{row}","color":"$(color)"}}\''
+      ], f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/special_render/grass_colored.mcfunction')
+      write_lines([
+         f'$data modify storage tryashtar.shulker_preview:data tooltip append value \'[{{"translate":"tryashtar.shulker_preview.layer.spawn_egg.0.{row}","color":"$(base)"}},{{"translate":"tryashtar.shulker_preview.layer.spawn_egg.1.{row}","color":"$(overlay)"}}]\''
+      ], f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/special_render/spawn_eggs.mcfunction')
+      write_lines([
+         'execute store success score #has_color shulker_preview store result score #color shulker_preview run data get storage tryashtar.shulker_preview:data item.components."minecraft:dyed_color".rgb',
+         f'$execute if items entity 7368756c-6b65-7220-7072-657669657721 contents wolf_armor run data modify storage tryashtar.shulker_preview:data tooltip append value \'[{{"translate":"tryashtar.shulker_preview.layer.$(id).0.{row}"}},{{"translate":"tryashtar.shulker_preview.layer.$(id).1.{row}","color":"#00aced"}}]\'',
+         f'$execute unless items entity 7368756c-6b65-7220-7072-657669657721 contents wolf_armor run data modify storage tryashtar.shulker_preview:data tooltip append value \'[{{"translate":"tryashtar.shulker_preview.layer.$(id).0.{row}","color":"#00aced"}},{{"translate":"tryashtar.shulker_preview.layer.$(id).1.{row}","color":"white"}}]\''
+      ], f'datapack/data/tryashtar.shulker_preview/functions/row_{row}/special_render/dyeable.mcfunction')
    grid_image = create_image(grid, 64)
    grid_image.save('resourcepack/assets/tryashtar.shulker_preview/textures/block_sheet.png', 'PNG')
    write_json(lang, 'resourcepack/assets/tryashtar.shulker_preview/lang/en_us.json')
@@ -117,7 +152,7 @@ def main():
 def color_hex(int_color):
    return f'#{format(int_color, '06x')}'
 
-def add_layered_translations(name, textures, lang, next_slot):
+def add_layered_translations(name, textures, lang, next_slot, overlay_offset):
    result = []
    for row in range(0, 3):
       sub = []
@@ -126,6 +161,8 @@ def add_layered_translations(name, textures, lang, next_slot):
          text = layer['rows'][row] + layer['negative']
          if layernum == len(textures) - 1:
             text += next_slot
+         else:
+            text += overlay_offset
          lang[key] = text
          sub.append(key)
       result.append(sub)
