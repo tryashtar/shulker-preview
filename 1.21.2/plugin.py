ded3f6ce1bf9355d175cbd5bc0c02f873ff0bb96
tryashtar <tryashtar@gmail.com>
2025-07-10 19:05:27 -0600

    Fix bee honey in 1.21.2
    Textures were getting loaded with the same name, overwriting the empty hive texture with the full one

--- 1.21.2/plugin.py
+++ 1.21.2/plugin.py
@@ -73,7 +73,6 @@ def main(ctx: beet.Context):
    lang['tryashtar.shulker_preview.missingno.2'] = missingno['rows'][2] + missingno['negative'] + next_slot
    special_render_tag = ['#tryashtar.shulker_preview:special_render/overrides']
    override_items = {}
-   block_model_maps = {'item/bee_nest_honey': 'bee_nest_full', 'item/beehive_honey': 'beehive_full'}
    hardcoded_items = {
       'grass_colored': {"vine":0x48b518,"lily_pad":0x71c35c,"short_grass":0x7bbd6b,"fern":0x7bbd6b,"tall_grass":0x7bbd6b,"large_fern":0x7bbd6b},
       'spawn_eggs': {f'{k}_spawn_egg':v for k,v in {"creaking":(0x5F5F5F, 16545810),"bogged":(9084018,3231003),"armadillo":(11366765,0x824848),"wither":(0x141414,5075616),"snow_golem":(14283506,8496292),"sniffer":(8855049,2468720),"iron_golem":(14405058,7643954),"ender_dragon":(0x1C1C1C,14711290),"camel":(16565097,13341495),"allay":(56063,44543),"axolotl":(16499171,10890612),"bat":(4996656,986895),"bee":(15582019,4400155),"blaze":(16167425,16775294),"breeze":(11506911,9529055),"cat":(15714446,9794134),"cave_spider":(803406,11013646),"chicken":(10592673,16711680),"cod":(12691306,15058059),"cow":(4470310,10592673),"creeper":(894731,0),"dolphin":(2243405,16382457),"donkey":(5457209,8811878),"drowned":(9433559,7969893),"elder_guardian":(13552826,7632531),"enderman":(1447446,0),"endermite":(1447446,7237230),"evoker":(9804699,1973274),"frog":(13661252,0xFFC77C),"fox":(14005919,13396256),"ghast":(16382457,12369084),"glow_squid":(611926,8778172),"goat":(10851452,5589310),"guardian":(5931634,15826224),"hoglin":(13004373,6251620),"horse":(12623485,15656192),"husk":(7958625,15125652),"llama":(12623485,10051392),"magma_cube":(3407872,16579584),"mooshroom":(10489616,12040119),"mule":(1769984,5321501),"ocelot":(15720061,5653556),"panda":(15198183,1776418),"parrot":(894731,16711680),"phantom":(4411786,8978176),"pig":(15771042,14377823),"piglin":(10051392,16380836),"piglin_brute":(5843472,16380836),"pillager":(5451574,9804699),"polar_bear":(0xEEEEDE,14014157),"pufferfish":(16167425,3654642),"rabbit":(10051392,7555121),"ravager":(7697520,5984329),"salmon":(10489616,951412),"sheep":(15198183,16758197),"shulker":(9725844,5060690),"silverfish":(7237230,3158064),"skeleton":(12698049,4802889),"skeleton_horse":(6842447,15066584),"slime":(5349438,8306542),"spider":(3419431,11013646),"squid":(2243405,7375001),"stray":(6387319,14543594),"strider":(10236982,5065037),"tadpole":(7164733,1444352),"trader_llama":(15377456,4547222),"tropical_fish":(15690005,16775663),"turtle":(15198183,44975),"vex":(8032420,15265265),"villager":(5651507,12422002),"vindicator":(9804699,2580065),"wandering_trader":(4547222,15377456),"warden":(1001033,3790560),"witch":(3407872,5349438),"wither_skeleton":(1315860,4672845),"wolf":(14144467,13545366),"zoglin":(13004373,15132390),"zombie":(44975,7969893),"zombie_horse":(3232308,9945732),"zombified_piglin":(15373203,5009705),"zombie_villager":(5651507,7969893)}.items()}
@@ -218,11 +217,11 @@ def main(ctx: beet.Context):
             chars = new_sprite(char_cache, False)
             char_cache['external'][appearance_hash] = (chars, f'render:{item}')
             result['base'].append(chars)
-      for override in model['overrides']:
+      for i,override in enumerate(model['overrides']):
          predicate = override['predicate']
          if not ('pulling' in predicate or 'pull' in predicate or 'brushing' in predicate or 'time' in predicate or 'angle' in predicate or 'cast' in predicate or 'tooting' in predicate or 'blocking' in predicate or 'trim_type' in predicate):
             submodel = get_model(vanilla.assets, model_cache, override['model'])
-            textures = load_textures(item, with_namespace(override['model']), submodel)
+            textures = load_textures(item + '.' + str(i), with_namespace(override['model']), submodel)
             result['overrides'].append((predicate, textures['base']))
       return result
    for item in item_list:
